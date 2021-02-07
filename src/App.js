c46605026acc6c9237e3f0d59a9693fee21e358c
DanielleMathey95 <danielle.mathey@yahoo.com>
2021-02-07 14:33:59 -0800

    MVP

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Character from "./components/Character";
+import "./App.css";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  const [characterInfo, setCharacterInfo] = useState([]);
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const getCharacters = () => {
+    axios.get('https://swapi.dev/api/people/?')
+    .then(response => {setCharacterInfo(response.data)})
+  }
+
+  useEffect(getCharacters, [])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+        characterInfo.map((character, index) => {
+          return <Character character={character} key={index}></Character>
+        })
+      }
     </div>
   );
-}
+};
 
 export default App;
+
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,45 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+const Character = (props) => {
+  const {character} = props
+  return (
+    <div className="container">
+      <div className="info">
+        <Card>
+          <Name>
+            <h3>{character.name}</h3>
+          </Name>
+          <Info>
+            <p>DOB: {character.birth_year}</p>
+            <p>Hair Color: {character.hair_color}</p>
+            <p>Height: {character.height}</p>
+            <p>Gender: {character.gender}</p>
+          </Info>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+const Card = styled.div`
+  background-color: black;
+  color: lightgrey;
+  width: 400px;
+  margin-left: auto;
+  margin-right: auto;
+  border-radius: 10%;
+  margin-bottom: 5%;
+`;
+
+const Name = styled.h3`
+  font-size: 1.5rem;
+  background: -webkit-linear-gradient(left, #081a8c, #29d606, #58088c);
+`;
+
+const Info = styled.p`
+  font-size: 20px;
+  color: #098496
+`
+
+export default Character;
